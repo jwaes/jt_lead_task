d8b7b4d295ef9dd192b5bfab2d6038023ac795d8
jwaes <jaco.waes@gmail.com>
2025-02-12 08:04:50 +0000

    moved from now_custom

--- models/project.py
+++ models/project.py
@@ -8,3 +8,17 @@ class ProjectTaskType(models.Model):
     _inherit = "project.task.type"
 
     ready_state = fields.Boolean(default=False)
+
+
+
+class CrmLeadTask(models.Model):
+    _inherit = 'project.task'
+
+    lead_id = fields.Many2one('crm.lead', tracking=True)
+    quotation_ready = fields.Boolean(default=False)
+
+    product_id = fields.Many2one(
+        'product.product', 'Related product',
+        check_company=True, index=True,
+        domain="['&',('type', 'in', ['product']),  '|', ('company_id', '=', False), ('company_id', '=', company_id)]",
+        help="A product that is the related to this task")
